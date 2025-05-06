446f2b23926d204c0ebdb95c100536dc7b420c75
Alex Tanti <atanti98@gmail.com>
2025-05-06 19:28:22 +0200

    extended make file

--- start_artnet_receiver.sh
+++ start_artnet_receiver.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# Script to start artnet_receiver on boot
-cd ~/rpi-gpu-hub75-matrix
 
-make lib
+# Script to start artnet_receiver on boot
+sudo apt update
+sudo apt upgrade
+
+# build dependencies for rpi-gpu-hub75-matrix
+sudo apt install build-essential gcc make libgles2-mesa-dev libgbm-dev libegl1-mesa-dev libavformat-dev libswscale-dev
+
+cd ./rpi-gpu-hub75-matrix
+
+# NOTE: you cannot compile for multiple boards. pin configuration is defined at compile time
+# compile with supports for hzeller's 3 port board (default):
+make DEF="-DHZELLER=1"
 
 sudo make install
 
